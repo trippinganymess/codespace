65a3d6f450179e8a5249c07f769be5e68b7f8d36
trippinganymess <135956311+trippinganymess@users.noreply.github.com>
2023-10-09 16:10:09 +0000

    Mon, Oct 9, 2023, 4:10 PM +00:00

---
+++ population/llamas.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(){
+int k = gtllms();
+printf("the current population of llamas is %i" k);
+}
+int gtllms(){
+    int n = get_int("what is the current population of llamas");
+}
